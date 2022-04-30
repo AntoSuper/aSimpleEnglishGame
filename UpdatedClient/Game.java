f7b06d905d7411eed78539fcaea52cbdca2633ba
Antonio Palladino <95444096+AntoSuper@users.noreply.github.com>
2022-04-30 12:44:28 +0200

    FIrst steps

--- UpdatedClient/Game.java
+++ UpdatedClient/Game.java
@@ -1,3 +1,25 @@
-public class Game {
+import java.awt.event.*;
+
+import java.awt.Dimension;
+import java.awt.Font;
+
+public class Game implements ActionListener {
     
+    private Dimension d;
+    private final Font font = new Font("Arial", Font.BOLD, 14);
+
+    public final static int BLOCK_SIZE = 24;
+
+
+
+
+
+
+    public Game () {
+
+    }
+
+    public void actionPerformed (ActionEvent e) {
+
+    }
 }
